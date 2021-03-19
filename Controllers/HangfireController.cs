652170c2438950803d200d70a546438e9a21b952
Muhammad Harith <harith781@gmail.com>
2021-03-19 14:48:00 +0800

    first commit

---
+++ Controllers/HangfireController.cs
@@ -0,0 +1,78 @@
+﻿using Hangfire;
+using Hangfire.Storage;
+using Hangfire.Storage.Monitoring;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hangfire_webApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangfireController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+
+        //fire & forget job
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Welcome()
+        {
+            var jobId = BackgroundJob.Enqueue(() => this.sendWelcome("Welcome to the background job"));
+
+            return Ok($"Job Id: {jobId}. Welcome email sent to the user");
+        }
+
+        //schedule job
+        [HttpPost]
+        [Route("[action]/{time}")]
+        public IActionResult Discount(int time)
+        {
+            var jobId = BackgroundJob.Schedule(() => this.sendWelcome("Welcome to the background job"), TimeSpan.FromSeconds(time));
+
+            return Ok($"Job Id: {jobId}. Discount email will be sent in {time}");
+        }
+
+        //recurring job
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult DatabaseUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database Update"), Cron.Minutely);
+
+            return Ok("Database check job initiated");
+        }
+
+        //continuous job
+        [HttpPost]
+        [Route("[action]/{time}")]
+        public IActionResult Confirm(int time)
+        {
+            var parentId = BackgroundJob.Schedule(() => Console.WriteLine("You asked to be unsubscribed"), TimeSpan.FromSeconds(time));
+            BackgroundJob.ContinueJobWith(parentId, () => Console.WriteLine("You were unsubscribed"));
+
+            return Ok("Confirmation job created");
+        }
+
+        [HttpGet("GetStorage/{jobId}")]
+        public IActionResult GetStorage(string jobId)
+        {
+            IMonitoringApi jobMonitoringApi = JobStorage.Current.GetMonitoringApi();
+            //var jobMonitoringApi = JobStorage.Current.GetConnection();
+            var job = jobMonitoringApi.FailedJobs(0, int.MaxValue);
+            return Ok();
+        }
+
+        public void sendWelcome(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Mongo;
+using Hangfire.Mongo.Migration.Strategies.Backup;
+using Hangfire.Mongo.Migration.Strategies;
+
+namespace hangfire_webApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var migrationOptions = new MongoMigrationOptions
+            {
+                MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                BackupStrategy = new CollectionMongoBackupStrategy()
+            };
+
+            services.AddHangfire(options => options.UseSqlServerStorage(Configuration.GetConnectionString("default")));
+            services.AddHangfireServer();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseHangfireDashboard();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
